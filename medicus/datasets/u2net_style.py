d7361e255c3ec3f03d24aaf341f778f894ef71b9
1ucky40nc3 <louisnwendler@gmail.com>
2022-03-28 20:00:43 +0200

    added u2net style dataset

---
+++ medicus/datasets/u2net_style.py
@@ -0,0 +1,70 @@
+from typing import Tuple
+from typing import List
+from typing import Optional
+from typing import Callable
+
+from PIL import Image
+
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+from .utils import list_dataset_files
+from .utils import set_seed
+
+
+class U2NetRecipeDataset:
+    def __init__(
+        self, 
+        sample_dir: str,
+        target_dir: str,
+        transform: Callable,
+        target_transform: Callable,
+        shared_transform: Callable,
+        share_transform_random_seed: bool = True
+    ) -> None:
+        samples_list, targets_list = list_dataset_files(
+            sample_dir, target_dir)
+
+        self.samples_list = samples_list
+        self.targets_list = targets_list
+        self.len = len(samples_list)
+
+        self.transform = transform
+        self.target_transform = target_transform
+        self.shared_transform = shared_transform
+        self.share_transform_random_seed = share_transform_random_seed
+
+    def __len__(self) -> int:
+        return self.len
+
+    def share_seed(self) -> bool:
+        return self.share_transform_random_seed
+
+    def __getitem__(
+        self, 
+        index: int
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        sample = self.samples_list[index]
+        target = self.targets_list[index]
+
+        sample = Image.open(sample).convert("RGB")
+        target = Image.open(target).convert("RGB")
+
+        if self.share_seed():
+            seed = np.random.randint(2147483647)
+            set_seed(seed)
+                    
+                
+        sample = self.shared_transform(sample)
+        input_sample = self.transform(sample)
+
+        if self.share_seed:
+            set_seed(seed)
+        
+        target = self.shared_transform(target)
+        target = self.target_transform(target)
+        
+        return input_sample, sample, target
