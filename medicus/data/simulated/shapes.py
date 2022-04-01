0a3259923425a3ba93688cc9c0ebfd8d74b8580b
1ucky40nc3 <louisnwendler@gmail.com>
2022-04-01 14:42:17 +0200

    added new ds generation func to disciminate shapes

--- medicus/data/simulated/shapes.py
+++ medicus/data/simulated/shapes.py
@@ -1,4 +1,5 @@
 from typing import Any
+from typing import Optional
 
 import os
 
@@ -36,8 +37,11 @@ def randn(n: int, scale: float = 1.) -> np.ndarray:
 
 
 def random_color(
-    class_color: str
+    class_color: Optional[str] = None
 ) -> str:
+    if class_color is None:
+        return np.random.choice(COLORS)
+    
     class_index = COLORS.index(class_color)
     p = np.ones(len(COLORS))
     p /= len(COLORS) - 1
@@ -64,12 +68,84 @@ def rectangle(
     )
 
 
+def square(
+    x: float,
+    y: float,
+    d: float,
+    b: float,
+    c: float,
+    a: float,
+    **kwargs
+) -> patches.Rectangle:
+    return patches.Rectangle(
+        xy=(x, y),
+        width=d,
+        height=d,
+        angle=b,
+        color=c,
+        alpha=a
+    )
+
+
+def polygon(
+    xy: np.ndarray,
+    c: str,
+    a: float,
+    closed: bool = True,
+    **kwargs
+) -> patches.Polygon:
+    return patches.Polygon(
+        xy=xy,
+        color=c,
+        alpha=a,
+        closed=closed,
+    )
+
+
+def circle(
+    x: float,
+    y: float,
+    r: float,
+    c: str,
+    a: float,
+    **kwargs
+) -> patches.Circle:
+    return patches.Circle(
+        xy=(x, y),
+        radius=r,
+        color=c,
+        alpha=a
+    )
+
+
+def wedge(
+    x: float,
+    y: float,
+    r: float,
+    t1: float,
+    t2: float,
+    w: float,
+    c: str,
+    a: float,
+    **kwargs
+) -> patches.Wedge:
+    return patches.Wedge(
+        center=(x, y),
+        r=r,
+        theta1=t1,
+        theta2=t2,
+        width=w,
+        color=c,
+        alpha=a
+    )
+
+
 def random_rect_args(
     width: int,
     height: int,
     min_rect_scale: float,
     max_rect_scale: float,
-    class_color: str,
+    class_color: Optional[str] = None,
     scale: float = 1.,
 ) -> dict:
     x, y = randn(2, scale)
@@ -81,7 +157,105 @@ def random_rect_args(
     w = w * width
     h = h * height
     a = 45 * randn(1)
+
     c = random_color(class_color)
+
+    return locals()
+
+
+def random_square_args(
+    width: int,
+    height: int,
+    min_square_scale: float = 0.1,
+    max_square_scale: float = 0.4,
+    class_color: Optional[str] = None,
+    scale: float = 1.,
+) -> dict:
+    x, y = randn(2, scale)
+    d = np.random.rand()
+    d = inside(d, min_square_scale, max_square_scale)
+    d *= min(width, height)
+
+    b = 45 * np.random.rand()
+    
+    c = random_color(class_color)
+    a = np.random.rand()
+    a = inside(a, 0.2, 0.8)
+
+    return locals()
+
+
+def random_polygon_args(
+    width: int,
+    height: int,
+    min_polygon_scale: float = 0.1,
+    max_polygon_scale: float = 0.7,
+    min_points: int = 3,
+    max_points: int = 8,
+    class_color: Optional[str] = None,
+) -> dict:
+    n = np.random.randint(min_points, max_points)
+    xy = np.random.rand(n, 2)
+    xy[:, 0] = xy[:, 0] * width
+    xy[:, 1] = xy[:, 1] * height
+    
+    s = np.random.rand()
+    s = inside(s, min_polygon_scale, max_polygon_scale)
+    xy *= s
+
+    c = random_color(class_color)
+    a = np.random.rand()
+    a = inside(a, 0.2, 0.8)
+
+    return locals()
+
+
+def random_circle_args(
+    width: int,
+    height: int,
+    min_circle_scale: float = 0.1,
+    max_circle_scale: float = 0.4,
+    class_color: Optional[str] = None,
+) -> dict:
+    x, y = np.random.rand(2)
+    x *= width
+    y *= height
+
+    r = np.random.rand()
+    r = inside(r, min_circle_scale, max_circle_scale)
+    r *= min(width, height)
+
+    c = random_color(class_color)
+    a = np.random.rand()
+    a = inside(a, 0.2, 0.8)
+
+    return locals()
+
+
+def random_wedge_args(
+    width: int,
+    height: int,
+    min_circle_scale: float = 0.1,
+    max_circle_scale: float = 0.4,
+    class_color: Optional[str] = None,
+) -> dict:
+    x, y = np.random.rand(2)
+    x *= width
+    y *= height
+
+    r = np.random.rand()
+    r = inside(r, min_circle_scale, max_circle_scale)
+    r *= min(width, height)
+
+    t1 = 360. * np.random.rand()
+    t2 = 360. * np.random.rand()
+
+    w = np.random.rand()
+    w = r * w
+
+    c = random_color(class_color)
+    a = np.random.rand()
+    a = inside(a, 0.2, 0.8)
 
     return locals()
 
@@ -99,7 +273,7 @@ def save_figure_axes(
 def generate_rectangles_samples(
     height: int,
     width: int,
-    num_samples: int,
+    num_samples: int = 20,
     num_rects: int = 20,
     class_color: str = "red",
     min_rect_scale: float = 0.1,
@@ -153,3 +327,80 @@ def generate_rectangles_samples(
 
 
 # generate_rectangles_samples(200, 200, 10)
+shapes_mapping = {
+    "square": {
+        "args": random_square_args,
+        "func": square
+    },
+    "polygon": {
+        "args": random_polygon_args,
+        "func": polygon
+    },
+    "circle": {
+        "args": random_circle_args,
+        "func": circle
+    },
+    "wedge": {
+        "args": random_wedge_args,
+        "func": wedge
+    }
+}
+
+
+def generate_shapes_samples(
+    height: int,
+    width: int,
+    num_samples: int,
+    num_shapes: int = 20,
+    class_shape: str = "square",
+    directory: str = ".",
+    file_prefix: str = "shapes",
+    file_type: str = "png",
+    seed: int = None
+) -> None:
+    if seed:
+        np.random.seed(seed)
+
+    inp_background = np.ones((height, width, 3))
+    tgt_background = np.zeros((height, width, 1))
+
+    os.makedirs(os.path.join(directory, "input"), exist_ok=True)
+    os.makedirs(os.path.join(directory, "target"), exist_ok=True)
+
+    other_shapes = list(shapes_mapping.keys())
+    other_shapes.remove(class_shape)
+
+    for i in range(num_samples):
+        fig, (inp, tgt) = plt.subplots(2)
+
+        inp.imshow(inp_background)
+        tgt.imshow(tgt_background)
+
+        for j in range(num_shapes - 1):
+            shape = np.random.choice(other_shapes)
+            args = shapes_mapping[shape]["args"]
+            func = shapes_mapping[shape]["func"]
+
+            args = args(width, height)
+            shape = func(**args)
+
+            inp.add_patch(shape)
+        
+        args = shapes_mapping[class_shape]["args"]
+        func = shapes_mapping[class_shape]["func"]
+        args = args(width, height)
+        
+        inp_args = {**args, "a": inside(args["a"], .6, 1.)}
+        tgt_args = {**args, "c": "white", "a": None}
+
+        inp.add_patch(func(**inp_args))
+        tgt.add_patch(func(**tgt_args))
+
+        inp_path = os.path.join(directory, "input", f"{file_prefix}_{i}.{file_type}")
+        tgt_path = os.path.join(directory, "target", f"{file_prefix}_{i}.{file_type}")
+
+        save_figure_axes(fig, inp, inp_path)
+        save_figure_axes(fig, tgt, tgt_path)
+
+
+generate_shapes_samples(200, 200, 10)
