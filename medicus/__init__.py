839dcb07e32c6fb48b2ccec7b071fcf0090f66f9
ufobob <69012292+ufobob@users.noreply.github.com>
2022-04-13 09:46:46 +0200

    Update __init__.py

--- medicus/__init__.py
+++ medicus/__init__.py
@@ -2,6 +2,4 @@ from .models.unet_backup import UNet
 from .data.utils import *
 #from .data.utils import list_dataset_files
 from .data.datasets import *
-from .scripts.train import *
-
-print("ok")
+from . import *
