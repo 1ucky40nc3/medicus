ef0566320fae26c7b819705e84844785e9c99a3c
ufobob <69012292+ufobob@users.noreply.github.com>
2022-04-14 14:40:54 +0200

    Update datasets.py
    Updated UnetDataset: Dateien können jetzt in einzelnen Patienten Ordnern gespeichert sein

--- medicus/data/datasets.py
+++ medicus/data/datasets.py
@@ -81,14 +81,22 @@ class UNetDataset:
         transform: Callable,
         target_transform: Callable,
         shared_transform: Callable,
-        share_transform_random_seed: bool = True
+        share_transform_random_seed: bool = True,
+        pat_dir: bool = False,
     ) -> None:
-        samples_list, targets_list = list_dataset_files(
-            sample_dir, target_dir)
+        #pat_dir -> True: für jeden Patienten existiert ein einzelner Unterordner, False: alle Dateien im gleichen Ordner
+        if(pat_dir):
+          samples_list, targets_list = list_dir_dataset_files(
+              sample_dir, target_dir)
+        else:
+          samples_list, targets_list = list_dataset_files(
+              sample_dir, target_dir)
 
         self.samples_list = samples_list
         self.targets_list = targets_list
         self.len = len(samples_list)
+
+        self.pat_dir = pat_dir
 
         self.transform = transform
         self.target_transform = target_transform
@@ -111,11 +119,17 @@ class UNetDataset:
         sample = Image.open(sample)
         target = Image.open(target)
 
+
+        if self.share_seed():
+            seed = np.random.randint(2147483647)
+            set_seed(seed)
                     
                 
         sample = self.shared_transform(sample)
         input_sample = sample
 
+        if self.share_seed:
+            set_seed(seed)
         
         target = self.shared_transform(target)
 
