df120d795710e2d618905baadb6b3f2d363b0e15
ufobob <69012292+ufobob@users.noreply.github.com>
2022-04-26 12:15:26 +0200

    corrected u2net

--- medicus/models/u2net.py
+++ medicus/models/u2net.py
@@ -417,8 +417,7 @@ class U2NET(nn.Module):
 
         d0 = self.outconv(torch.cat((d1,d2,d3,d4,d5,d6),1))
 
-        return F.sigmoid(d0)
-        #return F.sigmoid(d0), F.sigmoid(d1), F.sigmoid(d2), F.sigmoid(d3), F.sigmoid(d4), F.sigmoid(d5), F.sigmoid(d6)
+        return F.sigmoid(d0), F.sigmoid(d1), F.sigmoid(d2), F.sigmoid(d3), F.sigmoid(d4), F.sigmoid(d5), F.sigmoid(d6)
 
 
 ### U^2-Net small ###
