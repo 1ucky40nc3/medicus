9e62c37f136bccd09e6f2f8f2e125506a050f0e5
1ucky40nc3 <louisnwendler@gmail.com>
2022-10-05 21:11:58 +0200

    beautified code

--- medicus/data/nib_utils.py
+++ medicus/data/nib_utils.py
@@ -1,16 +1,25 @@
+from typing import Tuple
+
+import json
 from pathlib import Path
-from numpy.core.numeric import NaN
+
 import numpy as np
+from numpy.core.numeric import NaN
+
+from scipy.ndimage import center_of_mass
+
+from PIL import Image
+
 import nibabel as nib
 import nibabel.processing as nip
 import nibabel.orientations as nio
-from scipy.ndimage import center_of_mass
+
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, Normalize
 from matplotlib.patches import Circle
-import json
+
 from medicus.utils.affines import *
-from PIL import Image
+
 
 # define HU windows
 wdw_sbone = Normalize(vmin=-500, vmax=1300, clip=True)
@@ -20,18 +29,22 @@ wdw_hbone = Normalize(vmin=-200, vmax=1000, clip=True)
 # Resample and reorient #
 
 
-def reorient_to(img, axcodes_to=('P', 'I', 'R'), verb=False):
+def reorient_to(
+    img: nib.spatialimages.SpatialImage, 
+    axcodes_to: Tuple[str] = ('P', 'I', 'R'), 
+    verbose: bool = False
+) -> nib.nifti1.Nifti1Image:
     """Reorients the nifti from its original orientation to another specified orientation
     
-    Parameters:
-    ----------
-    img: nibabel image
-    axcodes_to: a tuple of 3 characters specifying the desired orientation
+    Args:
+        img (nibabel.spatialimages.SpatialImage): nibabel image
+        axcodes_to (tuple[str]): a tuple of 3 characters specifying the desired orientation
     
     Returns:
-    ----------
-    newimg: The reoriented nibabel image 
+        newimg (nib.nifti1.Nifti1Image): The reoriented nibabel image
     
+    Note:
+        Source: https://github.com/anjany/verse
     """
     aff = img.affine
     arr = np.asanyarray(img.dataobj, dtype=img.dataobj.dtype)
@@ -42,24 +55,28 @@ def reorient_to(img, axcodes_to=('P', 'I', 'R'), verb=False):
     aff_trans = nio.inv_ornt_aff(ornt_trans, arr.shape)
     newaff = np.matmul(aff, aff_trans)
     newimg = nib.Nifti1Image(arr, newaff)
-    if verb:
+    if verbose:
         print("[*] Image reoriented from", nio.ornt2axcodes(ornt_fr), "to", axcodes_to)
     return newimg
 
 
-def resample_nib(img, voxel_spacing=(1, 1, 1), order=3, verb = False):
+def resample_nib(
+    img: nib.spatialimages.SpatialImage,
+    voxel_spacing: Tuple[int] = (1, 1, 1),
+    order: int = 3,
+    verbose : bool = False):
     """Resamples the nifti from its original spacing to another specified spacing
     
-    Parameters:
-    ----------
-    img: nibabel image
-    voxel_spacing: a tuple of 3 integers specifying the desired new spacing
-    order: the order of interpolation
+    Args:
+        img (nib.spatialimages.SpatialImage): nibabel image
+        voxel_spacing (tuple[int]): a tuple of 3 integers specifying the desired new spacing
+        order (int): the order of interpolation
     
     Returns:
-    ----------
-    new_img: The resampled nibabel image 
-    
+        new_img: The resampled nibabel image
+
+    Note:
+        Source: https://github.com/anjany/verse 
     """
     # resample to new voxel spacing based on the current x-y-z-orientation
     aff = img.affine
@@ -70,102 +87,113 @@ def resample_nib(img, voxel_spacing=(1, 1, 1), order=3, verb = False):
         shp[0] * zms[0] / voxel_spacing[0],
         shp[1] * zms[1] / voxel_spacing[1],
         shp[2] * zms[2] / voxel_spacing[2]
-        ]).astype(int))
+    ]).astype(int))
     new_aff = rescale_affine(aff, shp, voxel_spacing, new_shp)
-    new_img = nip.resample_from_to(img, (new_shp, new_aff), order=order, cval=-1024)
-    if verb:
+    new_img = nip.resample_from_to(
+        img, 
+        (new_shp, new_aff), 
+        order=order, 
+        cval=-1024)
+    if verbose:
         print("[*] Image resampled to voxel size:", voxel_spacing)
     return new_img
 
 
-def resample_mask_to(msk, to_img, verb = False):
+def resample_mask_to(msk, to_img, verbose = False):
     """Resamples the nifti mask from its original spacing to a new spacing specified by its corresponding image
     
-    Parameters:
-    ----------
-    msk: The nibabel nifti mask to be resampled
-    to_img: The nibabel image that acts as a template for resampling
+    Args:
+        msk: The nibabel nifti mask to be resampled
+        to_img: The nibabel image that acts as a template for resampling
     
     Returns:
-    ----------
-    new_msk: The resampled nibabel mask 
+        new_msk: The resampled nibabel mask
     
+    Note:
+        Source: https://github.com/anjany/verse 
     """
     to_img.header['bitpix'] = 8
     to_img.header['datatype'] = 2  # uint8
     new_msk = nib.processing.resample_from_to(msk, to_img, order=0)
-    if verb:
+    if verbose:
         print("[*] Mask resampled to image size:", new_msk.header.get_data_shape())
     return new_msk
 
 
-def pad_and_crop(img, shape, const_value = 0):
+def pad_and_crop(
+    img: nib.spatialimages.SpatialImage,
+    shape: Tuple[int],
+    const_value: int = 0
+) -> nib.nifti1.Nifti1Image:
     """Shapes an image by padding and center cropping
     
-    Parameters:
-    ----------
-    img: nibabel image
-    shape: a tuple of 2 integers specifying the desired new shape in x/y direction
-    const_value: pads the image with const_value
+    Args:
+        img (nib.spatialimages.SpatialImage): nibabel image
+        shape (tuple[int]): a tuple of 2 integers specifying the desired new shape in x/y direction
+        const_value (int): pads the image with const_value
     
     Returns:
-    ----------
-    cropped_img: The nibabel image in the new shape
-    
+        cropped_img (nib.spatialimages.SpatialImage): The nibabel image in the new shape
+
+    Note:
+        Source: https://github.com/anjany/verse 
     """
 
     # Get image shape, x,y,z -> x is y is z is
 
-    x,y,z = img.shape
+    x,y, z = img.shape
     crop_x, crop_y = shape
     pad_x = x
     pad_y = y
     #pad_z = z
     padded_img = img
     if (x < crop_x) or (y < crop_y):
-      if (x < crop_x):
-        pad_x = crop_x
-      if (y < crop_y):
-        pad_y = crop_y
-      #if (z < crop_z):
-      #  pad_z = crop_z
+        if (x < crop_x):
+            pad_x = crop_x
+        if (y < crop_y):
+            pad_y = crop_y
 
-      add_x0 = (pad_x - x)//2
-      add_x1 = pad_x - x - add_x0
+        add_x0 = (pad_x - x) // 2
+        add_x1 = pad_x - x - add_x0
 
-      add_y0 = (pad_y - y)//2
-      add_y1 = pad_y - y - add_y0
-        
-      #add_z0 = (pad_z - z)//2
-      #add_z1 = pad_z - z - add_z0
+        add_y0 = (pad_y - y) // 2
+        add_y1 = pad_y - y - add_y0
 
-      pixel_array = img.get_fdata().copy()
-      image_pad = np.pad(pixel_array, [(add_x0,add_x1),(add_y0,add_y1),(0,0)], mode = 'constant', constant_values = const_value)
+        pixel_array = img.get_fdata().copy()
+        image_pad = np.pad(
+            pixel_array, 
+            [
+                (add_x0, add_x1),
+                (add_y0, add_y1),
+                (0     , 0     )
+            ], 
+            mode='constant', 
+            constant_values=const_value
+        )
 
-      padded_img = nib.Nifti1Image(image_pad, img.affine, img.header)
+        padded_img = nib.Nifti1Image(image_pad, img.affine, img.header)
     
     start_x = pad_x//2 - (crop_x//2)
     start_y = pad_y//2 - (crop_y//2)
-    #start_z = pad_z//2 - (crop_z//2)
-
-
 
     cropped_img = padded_img.slicer[start_x : start_x + crop_x, start_y : start_y + crop_y,:] 
     return cropped_img
 
 
-def nifti_to_png(img, target_dir, start_index = 0):
+def nifti_to_png(
+    img: nib.spatialimages.SpatialImage,
+    target_dir: str,
+    start_index: int = 0
+) -> None:
     """Creates png image from nifti-file
     
-    Parameters:
-    ----------
-    img: nibabel image
-    target_dir: directiory where the images will be saved
-    start_index: first index in naming the files
-
-    Returns:
-    ----------
+    Args:
+        img (nib.spatialimages.SpatialImage): nibabel image
+        target_dir (str): directiory where the images will be saved
+        start_index (int): first index in naming the files
     
+    Note:
+        Source: https://github.com/anjany/verse 
     """
     pixel_3d = np.array(img.get_fdata())
     for i, pixel_2d in enumerate(pixel_3d):
