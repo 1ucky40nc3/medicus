4da93d65c9bba264a7cfb1dbcd71c0790778df66
1ucky40nc3 <louisnwendler@gmail.com>
2022-10-10 20:55:31 +0200

    added inference file for inference implementation

---
+++ medicus/inf.py
@@ -0,0 +1,58 @@
+from typing import Any
+from typing import Dict
+from typing import Tuple
+from typing import Callable
+from typing import Optional
+from typing import List
+
+import os
+import sys
+import math
+import json
+import logging
+
+from tqdm import tqdm
+
+import numpy as np
+
+import torch.nn as nn
+from torch.utils import tensorboard
+from torchmetrics import MeanMetric
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from torchsummary import summary
+from colour import Color
+
+import torchvision.transforms.functional as F
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as TNF
+
+import torchvision
+
+import time
+import copy
+
+from collections import defaultdict
+
+import wandb
+
+Device = Any
+LRScheduler = Any
+
+from .objectives.unet import dice_loss
+from .utils import timestamp, parse, inference, evaluate, Writer, masks2imgs
+
+
+def inference(
+    model: nn.Module,
+    samples: torch.Tensor,
+    device: Device
+) -> Tuple[torch.Tensor]:
+    with torch.no_grad():
+        samples = samples.to(device)
+        outputs = model(samples)
+
+    return outputs
